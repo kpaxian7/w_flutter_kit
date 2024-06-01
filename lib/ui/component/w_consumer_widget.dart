81fec60b0ad8d503ba65ca9e957132e5a7735084
zelong <smxldm@163.com>
2024-06-01 20:44:38 +0800

    modify：consumer widget

--- lib/ui/component/w_consumer_widget.dart
+++ lib/ui/component/w_consumer_widget.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+abstract class WConsumerWidget<P extends ChangeNotifier>
+    extends StatelessWidget {
+  const WConsumerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<P>(
+      builder: (ctx, vm, child) {
+        return buildBodyWidget(context, vm);
+      },
+    );
+  }
+
+  Widget buildBodyWidget(BuildContext context, P viewModel);
+}
